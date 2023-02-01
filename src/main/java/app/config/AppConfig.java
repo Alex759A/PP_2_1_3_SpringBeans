1aad41141ddb077e83d5f2a78e4321b89707a4bb
Alex759A <brelevskiyaleksey@gmail.com>
2023-02-01 17:30:30 +0600

    1.4

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,19 +1,13 @@
 package app.config;
 
-import app.model.Timer;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
-//    @Bean(name = "timer")
-//    public Long getTime() {
-//        Timer timer = new Timer();
-//        Long timerResult = timer.getTime();
-//        return timerResult;
-//    }
+
 }
 
 /*
--- src/main/java/app/model/Timer.java
+++ src/main/java/app/model/Timer.java
@@ -1,7 +1,6 @@
 package app.model;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Scope;
+
 import org.springframework.stereotype.Component;
 
 
